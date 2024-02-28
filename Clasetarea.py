dd15921f368d3b1e838782f4b88ff356f712102d
ghuali <guaghuali@gmail.com>
2024-02-28 09:29:23 +0000

    añadido constructor

--- Clasetarea.py
+++ Clasetarea.py
@@ -1,12 +1,12 @@
 class alumno:
-    def imprimir(self):
+    def __init__(self):
         self.nota = 0
         self.nombre = ""
+    
+    def imprimir(self):
         print(self.nota,self.nombre)
 
     def promociona(self):
-        self.nota = 0
-        self.nombre = 0
         if self.nota >= 5:
             return True
         else:
