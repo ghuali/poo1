f8d3d0175b9c92fcde041535d6c2e6fafd6216a8
ghuali <guaghuali@gmail.com>
2024-02-28 09:26:42 +0000

    update

--- Clasetarea.py
+++ Clasetarea.py
@@ -1,29 +1,15 @@
 class alumno:
-    def nota(self):
+    def imprimir(self):
         self.nota = 0
-        return self.nota
-
-    def nombre(self):
         self.nombre = ""
-        return self.nombre
-    
+        print(self.nota,self.nombre)
+
     def promociona(self):
+        self.nota = 0
+        self.nombre = 0
         if self.nota >= 5:
             return True
         else:
             return False
 
 
-
-def main():
-    Alumno1 = alumno()
-    Alumno1.nota = 6
-    Alumno1.nombre = "Ángel"
-    Alumno1.promociona
-
-    
-
-
-
-if __name__ == "__main__":
-    main()
