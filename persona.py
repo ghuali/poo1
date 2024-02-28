d8fc29dac06bca08987c34e403f8c4f950d86f4d
ghuali <guaghuali@gmail.com>
2024-02-28 09:52:36 +0000

    Ejercicio hecho(no checkeo)

---
+++ persona.py
@@ -0,0 +1,20 @@
+class persona:
+    def __init__(self,nombre,edad):
+        self.nombre = nombre
+        self.edad = edad
+    
+    def imprimir(self):
+        print(self.nombre,self.edad)
+
+    def cumpleaños(self):
+        cumple = self.edad + 1
+        print(cumple)
+
+
+    def main():
+        Aday = persona("Aday",19)
+        Aday.imprimir()
+        Aday.cumpleaños()
+
+
+    main()
