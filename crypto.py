a6e2b5a998e6219ce33ccc7a99851750958f7b2b
ghuali <guaghuali@gmail.com>
2024-03-01 09:37:47 +0000

    Stoploss crypto

--- crypto.py
+++ crypto.py
@@ -2,6 +2,8 @@ class Crypto:
     def __init__(self, nombre:str, euros:float) -> None:
         self.nombre = nombre
         self.euros = euros
+        self.min = 0.
+        self.max = 0.
 
     def stoploss(self, min:float, max:float):
         self.min = min
@@ -9,9 +11,11 @@ class Crypto:
 
     def imprimir(self):
         print("El valor de",self.nombre,"tiene de valor de",self.euros)
+        print("Stop Loss, Min: ",self.min,"Stop Loss max: ",self.max)
 
 
 if __name__ == '__main__':
     bitcoin = Crypto("bitcoin", 57000)
+    bitcoin.stoploss(55000, 60000)
     bitcoin.imprimir()
         
